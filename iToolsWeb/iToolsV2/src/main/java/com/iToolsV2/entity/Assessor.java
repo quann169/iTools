e023118a3202e5af633f72263b6268dded003065
tanthuong <ngngoctanthuong@gmail.com>
2018-06-13 22:16:04 +0700

    iToolsWebsite version 1

---
+++ iToolsWeb/iToolsV2/src/main/java/com/iToolsV2/entity/Assessor.java
@@ -0,0 +1,174 @@
+package com.iToolsV2.entity;
+ 
+import java.io.Serializable;
+ 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+ 
+@Entity
+@Table(name = "Assessor", //
+		uniqueConstraints = { //
+				@UniqueConstraint(columnNames = "UserName") })
+public class Assessor implements Serializable {
+ 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -7010154086721560047L;
+	public static final String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
+    
+    @Id
+    @Column(name = "AssessorID", nullable = false)
+    private int assessorID;
+    
+    @Column(name = "UserName", length = 100, nullable = false)
+    private String userName;
+    
+    @Column(name = "FingerID", length = 100, nullable = false)
+    private String fingerID;
+ 
+    @Column(name = "Password", length = 255, nullable = false)
+    private String encrytedPassword;
+    
+    @Column(name = "FirstName", length = 255, nullable = false)
+    private String firstName;
+    
+    @Column(name = "LastName", length = 255, nullable = false)
+    private String lastName;
+    
+    @Column(name = "EmailAddress", length = 255, nullable = false)
+    private String emailAddress;
+    
+    @Column(name = "Address", length = 255, nullable = false)
+    private String address;
+    
+    public int getAssessorID() {
+		return assessorID;
+	}
+
+	public void setAssessorID(int assessorID) {
+		this.assessorID = assessorID;
+	}
+
+	public String getFingerID() {
+		return fingerID;
+	}
+
+	public void setFingerID(String fingerID) {
+		this.fingerID = fingerID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCompanyCode() {
+		return companyCode;
+	}
+
+	public void setCompanyCode(String companyCode) {
+		this.companyCode = companyCode;
+	}
+
+	public String getLastPassword() {
+		return lastPassword;
+	}
+
+	public void setLastPassword(String lastPassword) {
+		this.lastPassword = lastPassword;
+	}	
+
+	@Column(name = "Phone", length = 255, nullable = false)
+    private String phone;
+    
+    @Column(name = "CompanyId", length = 100, nullable = false)
+    private String companyCode;
+    
+    @Column(name = "LastPassword", length = 255, nullable = false)
+    private String lastPassword;
+ 
+    @Column(name = "IsActive", length = 1, nullable = false)
+    private boolean active;
+ 
+    /*@Column(name = "IsFirstChange", length = 1, nullable = true)
+    private boolean isFirstChange;
+    
+    public boolean isFirstChange() {
+		return isFirstChange;
+	}
+
+	public void setFirstChange(boolean isFirstChange) {
+		this.isFirstChange = isFirstChange;
+	}*/
+ 
+    public String getUserName() {
+        return userName;
+    }
+ 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+ 
+    public String getEncrytedPassword() {
+        return encrytedPassword;
+    }
+ 
+    public void setEncrytedPassword(String encrytedPassword) {
+        this.encrytedPassword = encrytedPassword;
+    }
+ 
+    public boolean isActive() {
+        return active;
+    }
+ 
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+ 
+    @Override
+    public String toString() {
+        return "[" + this.userName + "," + this.encrytedPassword + "]";
+    }
+ 
+}
---
+++ iToolsWeb/iToolsV2/src/main/java/com/iToolsV2/entity/RoleAssessor.java
@@ -0,0 +1,107 @@
+package com.iToolsV2.entity;
+ 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+ 
+@Entity
+@Table(name = "RoleAssessor", //
+		uniqueConstraints = { //
+        @UniqueConstraint(columnNames = { "AssessorID", "RoleID" }) })
+public class RoleAssessor implements Serializable {
+ 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4226993732257359366L;
+
+	@Id
+    @Column(name = "RoleAssessorID", nullable = false)
+    private int roleAssessorID;
+ 
+/*    @Column(name = "RoleID", nullable = true)
+    private int RoleID;
+    
+    @Column(name = "AssessorID", nullable = true)
+    private int AssessorID;*/
+    
+	@Column(name = "IsActive", length = 1, nullable = false)
+    private boolean active;
+ 
+    @Column(name = "CreatedDate", nullable = true)
+    private Date createdDate;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "AssessorID", nullable = false)
+    private Assessor assessor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RoleID", nullable = false)
+    private Roles roles;
+ 
+    public int getRoleAssessorID() {
+		return roleAssessorID;
+	}
+
+	public void setRoleAssessorID(int roleAssessorID) {
+		this.roleAssessorID = roleAssessorID;
+	}
+
+/*	public int getRoleID() {
+		return RoleID;
+	}
+
+	public void setRoleID(int roleID) {
+		RoleID = roleID;
+	}
+
+	public int getAssessorID() {
+		return AssessorID;
+	}
+
+	public void setAssessorID(int assessorID) {
+		AssessorID = assessorID;
+	}*/
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Assessor getAssessor() {
+		return assessor;
+	}
+
+	public void setAssessor(Assessor assessor) {
+		this.assessor = assessor;
+	}
+
+	public Roles getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Roles roles) {
+		this.roles = roles;
+	}
+ 
+}
